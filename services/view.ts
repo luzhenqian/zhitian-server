a361b5e2c873625647306be5a9a6cd3e302f6d99
luzhenqian <15753140326@163.com>
2021-08-09 17:04:55 +0800

    fix: view service

--- services/view.ts
+++ services/view.ts
@@ -3,24 +3,33 @@ import { find, insert, remove, update } from "../repository/view";
 import { ICommonError, NewError } from "./common";
 
 export async function viewInsert(view: View): Promise<ICommonError | any> {
-  const result = await insert(view);
-  if (result instanceof Error) return NewError(result.message, 500);
-  return result;
+  try {
+    const result = await insert(view);
+    return result;
+  } catch (err) {
+    return NewError(err.message, 500);
+  }
 }
 
 export async function viewFind(view: ViewUpdate): Promise<ICommonError | any> {
-  const result = await find(view);
-  if (result instanceof Error) return NewError(result.message, 500);
-  return result;
+  try {
+    const result = await find(view);
+    return result;
+  } catch (err) {
+    return NewError(err.message, 500);
+  }
 }
 
 export async function viewUpdate(
   id: string,
   view: ViewUpdate
 ): Promise<ICommonError | any> {
-  const result = await update(id, view);
-  if (result instanceof Error) return NewError(result.message, 500);
-  return result;
+  try {
+    const result = await update(id, view);
+    return result;
+  } catch (err) {
+    return NewError(err.message, 500);
+  }
 }
 
 export async function viewRemove(id: string): Promise<ICommonError | any> {
